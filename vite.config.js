5c30869570e7281a29c32e9caac9140f23905fbf
phulengo <quainhanp24@gmail.com>
2022-09-04 02:19:47 +0700

    fix: fix vite config, production build path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,26 @@
-const path = require('path')
+const path = require("path");
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, "src"),
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        music_distribution: path.resolve(
+          __dirname,
+          "src/music-distribution.html"
+        ),
+      },
+    },
   },
   server: {
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+};
