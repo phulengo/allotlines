3448023cacc1208dff4ec782f6cd77232249aabe
phulengo <quainhanp24@gmail.com>
2022-09-04 17:59:37 +0700

    fix: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,12 @@ export default {
           __dirname,
           "src/music-distribution.html"
         ),
+        pricing: path.resolve(__dirname, "src/pricing.html"),
+        label: path.resolve(__dirname, "src/label.html"),
+        stories: path.resolve(__dirname, "src/stories.html"),
+        qa: path.resolve(__dirname, "src/qa.html"),
+        faq: path.resolve(__dirname, "src/faq.html"),
+        promotion: path.resolve(__dirname, "src/promotion.html"),
       },
     },
   },
